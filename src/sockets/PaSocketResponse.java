bc237d333a3b0389253a3fa9f284933c7c8bb693
sfricker <fricker.sebastien@orange.fr>
2016-05-22 00:34:34 +0200

    Adding PaSocketResponse class to handle socket response from server

---
+++ src/sockets/PaSocketResponse.java
@@ -0,0 +1,69 @@
+package sockets;
+
+import errors.PaErrors;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class PaSocketResponse extends PaSocketMessage implements Serializable {
+
+    private ArrayList<PaSocketResponseError> errors;
+    private Object content;
+
+    /**
+     * @return the errors
+     */
+    public ArrayList getErrors() {
+        return errors;
+    }
+
+    /**
+     * @param errors the errors to set
+     */
+    public void setErrors(ArrayList errors) {
+        this.errors = errors;
+    }
+
+    public void addError(PaSocketResponseError error) {
+        this.errors.add(error);
+    }
+    
+    public void addError(PaErrors message) {
+        addError(message.toString());
+    }
+    
+    public void addError(String message) {
+        PaSocketResponseError error;
+        
+        error = new PaSocketResponseError();
+        error.setCode(0);
+        error.setMessage(message);
+        error.setCriticity(PaSocketResponseErrorCriticity.NORMAL);
+        
+        this.errors.add(error);        
+    }  
+
+    /**
+     * @return the content
+     */
+    public Object getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    public PaSocketResponse() {
+        this.errors  = new ArrayList();
+        this.content = null;
+    }
+
+    public PaSocketResponse(PaSocketAction pAction) {
+        super(pAction);
+        this.errors  = new ArrayList();
+        this.content = null;
+    }
+}
