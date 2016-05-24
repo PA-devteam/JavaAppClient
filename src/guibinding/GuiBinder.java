995f33fa8ceaee8c762aa35b6be399503b8807fa
Khensit90 <romain.spenlihauer@gmail.com>
2016-05-24 15:28:51 +0200

    Adding a GuiBinder Class to store GUI FXML binding references

---
+++ src/guibinding/GuiBinder.java
@@ -0,0 +1,9 @@
+package guibinding;
+
+import entities.*;
+import java.util.ArrayList;
+
+public class GuiBinder {
+    public static UserProperty user       = new UserProperty();
+    public static ArrayList<String> roles = new ArrayList<>();
+}
