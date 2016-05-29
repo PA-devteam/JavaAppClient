000be137aaaddbe9496c14a330e3619f8d732dec
sfricker <fricker.sebastien@orange.fr>
2016-05-29 18:11:59 +0200

    Refactoring top navbar userinfos

--- src/screens/FXMLHeaderInfosController.java
+++ src/screens/FXMLHeaderInfosController.java
@@ -9,6 +9,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import security.Authenticator;
 import guibinding.GuiBinder;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import notifications.NotificationsManager;
 
 public class FXMLHeaderInfosController implements Initializable {
 
@@ -23,6 +27,9 @@ public class FXMLHeaderInfosController implements Initializable {
 
     @FXML
     private Label userRoles;
+    
+    @FXML
+    private Button btnMessages;
 
     /**
      * Initializes the controller class.
@@ -41,4 +48,29 @@ public class FXMLHeaderInfosController implements Initializable {
         // Binding user avatar
         avatarHolder.imageProperty().bind(GuiBinder.user.getUserAvatarImage());
     }
+    
+    @FXML
+    private void handleClick(ActionEvent event) throws IOException {
+        Button mItem = (Button) event.getSource();
+
+        String btnId = mItem.getId();
+
+        switch (btnId) {
+            case "btnNotifications":
+                System.out.println("Showing notifications");
+                NotificationsManager.info("Notifications", "Fonctionnalité en cours de développement");
+                break;
+            case "btnChat":
+                System.out.println("Showing chat");
+                NotificationsManager.info("Tchat", "Fonctionnalité en cours de développement");
+                break;
+            case "btnMessages":
+                System.out.println("Showing messages");
+                NotificationsManager.info("Messagerie", "Fonctionnalité en cours de développement");
+                break;                
+            default:
+                System.out.println("Action '" + btnId + "' is not a valid");
+                break;
+        }
+    }    
 }
